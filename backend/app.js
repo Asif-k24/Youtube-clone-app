63742c67641a6f3af7a5ac2b464fd331edf575ad
asif <asif69284@gmail.com>
2025-12-22 20:35:46 +0000

    fix CORS configuration

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://192.168.56.101:5173"],
     credentials: true,
   })
 );
